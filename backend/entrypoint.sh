2b7a839e75d6f1ee312d498e556bdbc960cb1af3
Salah Moustafa <153738101+salahamran@users.noreply.github.com>
2025-07-16 15:03:47 +0300

    update the main.yml locally

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput --clear
 cp -r /app/collected_static/. /staticfiles/
 cp -r /app/collected_static/. /backend_static/static/
 
-exec gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+exec "$@"
